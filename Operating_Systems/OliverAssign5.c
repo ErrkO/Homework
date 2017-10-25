7f0822ea7f5ea6239b5388d19cbd31059ea562be
errko <eoliver4@my.apsu.edu>
2017-10-25 10:42:25 -0500

    Fuck OCaml

--- Operating_Systems/OliverAssign5.c
+++ Operating_Systems/OliverAssign5.c
@@ -1,5 +1,11 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
 
 // Eric Oliver
 // CSCI 4100
@@ -7,8 +13,8 @@
 
 // This program is a basic shell that executes linux commands
 
-#define MAX_LINE = 100;
-#define MAX_ARGS = 2;
+#define MAX_LINE = 256;
+#define MAX_ARGS = 64;
 
 int main( int argc, char *argv[])
 {
@@ -50,42 +56,80 @@ int main( int argc, char *argv[])
 
 }
 
-void Read_Line(char line[])
+void Read_Line(char *line)
 {
 
-	char temp[];
+	char c;
 
-	for (int i = 0; i < MAX_LINE; i++)
-	{
+  	int chars_read = 0;
 
-		if (strcmp(line,space) == 0)
-		{
+  	while((c = getchar()) != '\n' && chars_read < MAX_LINE)
+  	{
 
-			
+    	line[chars_read++] = c;
 
-		}
+    }
 
-	}
+  	line[chars_read] = '\0';
 
 }
 
-int Is_Blank(char line[])
+int Is_Blank(const char *line)
 {
 
-	if (strcmp(line,"") == 0 || strcmp(line," ") == 0)
-	{
+	int i = 0;
 
-		return 1;
+  	while(line[i] != '\0') 
+  	{
 
-	}
+    	if(!isspace(line[i++]))
+    	{
 
-	return 0;
+      		return 0;
+
+      	}
+
+  	}
+  return 1;
 
 }
 
-void Parse_Args(char command, char argv)
+void Parse_Args(char *command, char *argv[])
 {
 
+	int argc = 0;
 
+  	while(*command != '\0' && argc < MAX_ARGS) 
+  	{
+
+    	int i = 0;
+
+    	while(!isspace(command[i]) && command[i] != '\0') i++;
+
+    	argv[argc] = (char *) malloc(i + 1);
+
+    	strncpy(argv[argc], command, i);
+
+    	while(isspace(command[i])) i++;
+
+    	command += i;
+
+    	argc++;
+
+  	}
+
+  	if(argc < MAX_ARGS)
+  	{
+
+    	argv[argc] = NULL;
+
+    }
+
+  	else
+  	{
+
+    	argv[MAX_ARGS - 1] = NULL;
+
+    }
 
 }
--- Programming_Languages/Ocaml/OliverAssign5.ml
+++ Programming_Languages/Ocaml/OliverAssign5.ml
@@ -2,6 +2,18 @@
 (*CSCI 4230 *)
 (*First Ocaml assignment*)
 
+(*#use "OliverAssign5.ml";;*)
+
+let t1 = [1;2;3;4;5;6;8;10];;
+
+let t2 = [2;4;6;7;8;9;10;11];;
+
+let t3 = [1;2;3;4;5;6;7;8;9];;
+
+let t4 = [6;2;8;1;4;9;7;3;5];;
+
+let t5 = [3;2;1];;
+
 let sqaure x = x *. x;;
 
 let rad a b c = (sqaure b -. 4.0 *. a *. c );;
@@ -20,12 +32,58 @@ let third (_,_,a) = a;;
 let reverse lst = 
 	let rec rev_help acc = function
 	| [] -> acc
-	| h::t -> rev_help (h::acc) t in
-	rev_help [] lst;;
+	| f::b -> rev_help (f::acc) b in
+		rev_help [] lst;;
 
 let member item lst =
-	let rec mem_help acc = function
-	| [] -> acc
-	| h::t -> if h = item then true
-		else (mem_help c);;
+	let rec mem_help item lst =
+		match lst with
+		| [] -> false
+		| f::b -> if f = item then true
+			else mem_help item b
+		in mem_help item lst;;
 
+let union lst lst2 =
+	let rec un_help lst lst2 =
+		match lst, lst2 with
+		| [],_ -> lst2
+		| _,[] -> lst
+		| fx::bx, fy::by ->
+			if member fx lst2 then un_help bx lst2 else fx :: un_help bx lst2
+		in un_help lst lst2;;
+
+
+let intersect lst lst2 =
+	let rec inter_help lst lst2 =
+		match lst with
+		| [] -> lst
+		| fx::bx ->
+			if member fx lst2 then fx :: inter_help bx lst2 else inter_help bx lst2
+		in inter_help lst lst2;;
+
+let lessthen pivot lst =
+	let rec less_help pivot lst =
+		match lst with
+		| [] -> lst
+		| f::b -> if f < pivot then f :: less_help pivot b
+			else less_help pivot b
+		in less_help pivot lst;;
+
+let greaterthan pivot lst =
+	let rec great_help pivot lst =
+		match lst with
+		| [] -> lst
+		| f::b -> if f >= pivot then f :: great_help pivot b
+			else great_help pivot b
+		in great_help pivot lst;;
+
+let partition pivot lst =
+	(lessthen pivot lst , (greaterthan pivot lst));;
+
+let quicksort lst =
+	let rec quick_help lst =
+	match lst with
+    | [] -> lst
+    | f::b -> let tup = partition f b in
+    	quicksort (fst tup) @ [f] @ quicksort (snd tup)
+    in quick_help lst;;
