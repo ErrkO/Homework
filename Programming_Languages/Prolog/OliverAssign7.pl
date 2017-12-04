10a8545059f461bd8d9619a7315916ee8c0f3299
errko <eoliver4@my.apsu.edu>
2017-12-04 15:23:35 -0600

    worked on prolog some more

--- Programming_Languages/Prolog/OliverAssign7.pl
+++ Programming_Languages/Prolog/OliverAssign7.pl
@@ -1,13 +1,45 @@
+% Eric Oliver
+% CSCI 4230
+% Assignment 7
+
+% returns true if M is female and a parent of C
 mother(M,C) :- female(M) , parent(M,C).
 
+% returns true if F is male and a parent of C
 father(F,C) :- male(F), parent(F,C).
 
+% returns true if s1 and s2 share the same parent
 sibling(S1,S2) :- parent(P,S1), parent(P,S2).
 
+% returns true if C1 and C2 are first cousins
 first_cousin(C1,C2) :- parent(P1,C1), sibling(P1,P2), parent(P2,C2).
 
+% returns true if D descends from A
 ancestor(A,D) :- parent(A,D).
 ancestor(A,D) :- parent(A,B), ancestor(C,D).
 
-common_ancestor(A,P1,P2) :- first_cousin(P1,P2).
-common_ancestor(A,P1,P2) :- first_cousin(B,C), parent(C,P2), parent(B,P1), common_ancestor(A,B,C).
+%common_ancestor(A,P1,P2) :- first_cousin(P1,P2).
+%common_ancestor(A,P1,P2) :- first_cousin(B,C), parent(C,P2), parent(B,P1), common_ancestor(A,B,C).
+
+% returns true if P1 and P2 share A as an ancestor
+common_ancestor(A,P1,P2) :- not(P1 = P2), ancestor(A,P1), ancestor(A,P2).
+
+% returns true if R is the reverse of L
+do_reverse(L, R) :- do_reverse(L, R, []).
+
+do_reverse([], L, L).
+do_reverse([H|T], L, TR) :- do_reverse(T, L, [H|TR]).
+
+% insert
+
+% insertion sort
+
+% returns true if L3 is a union of L1 and L2
+is_union([], L2, L3).
+is_union([H|L1t, L2, L3) :- memberchk(H, L2), !, is_union(L1t, L2, L3).
+is_union([H|L1t], L2, [H|L3t]) :- is_union(L1t, L2, L3t).
+
+% returns true if L3 is an intersection of L1 and L2
+is_intersection([], _, []).
+is_intersection([H|L1t], L2, L3) :- memberchk(H, L2), !, L3 = [H|L3t],is_intersection(L1t, L2, L3t).
+is_intersection([_|L1t], L2, L3) :- is_intersection(L1t, L2, L3).
